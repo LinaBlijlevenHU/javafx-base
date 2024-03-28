e48e5c9ed25878ef74a181676858df174cee7c53
Lina Blijleven <lina.blijleven@hu.nl>
2024-03-28 17:05:05 +0100

    Setting up dependencies with the correct versions

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module com.example.javafxbase {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.javafxbase to javafx.fxml;
     exports com.example.javafxbase;
+    exports com.example.javafxbase.userinterface;
+    opens com.example.javafxbase.userinterface to javafx.fxml;
 }
